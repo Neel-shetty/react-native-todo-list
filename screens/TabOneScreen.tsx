553fc5742c8189584ecf5e7fd0b1111d69e21077
Neel <neelnarayanshetty@gmail.com>
2023-01-09 18:16:54 +0530

    redux toolkit

--- screens/TabOneScreen.tsx
+++ screens/TabOneScreen.tsx
@@ -1,15 +1,29 @@
-import { StyleSheet } from 'react-native';
+import { Button, StyleSheet } from "react-native";
+import { useSelector, useDispatch } from 'react-redux'
+import EditScreenInfo from "../components/EditScreenInfo";
+import { Text, View } from "../components/Themed";
+import { RootTabScreenProps } from "../types";
+import { decrement, increment } from "../state/counterslice";
+import type { RootState } from "../state/redux";
 
-import EditScreenInfo from '../components/EditScreenInfo';
-import { Text, View } from '../components/Themed';
-import { RootTabScreenProps } from '../types';
+export default function TabOneScreen({
+  navigation,
+}: RootTabScreenProps<"TabOne">) {
+  const count = useSelector((state: RootState) => state.counter.value)
+  const dispatch = useDispatch()
 
-export default function TabOneScreen({ navigation }: RootTabScreenProps<'TabOne'>) {
   return (
     <View style={styles.container}>
       <Text style={styles.title}>Tab One</Text>
-      <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
+      <View
+        style={styles.separator}
+        lightColor="#eee"
+        darkColor="rgba(255,255,255,0.1)"
+      />
       <EditScreenInfo path="/screens/TabOneScreen.tsx" />
+      <Text>{count}</Text>
+      <Button title="increase" onPress={()=> dispatch(increment())}/>
+      <Button title="decrease" onPress={()=> dispatch(decrement())}/>
     </View>
   );
 }
@@ -17,16 +31,16 @@ export default function TabOneScreen({ navigation }: RootTabScreenProps<'TabOne'
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
+    alignItems: "center",
+    justifyContent: "center",
   },
   title: {
     fontSize: 20,
-    fontWeight: 'bold',
+    fontWeight: "bold",
   },
   separator: {
     marginVertical: 30,
     height: 1,
-    width: '80%',
+    width: "80%",
   },
 });
--- screens/TabTwoScreen.tsx
+++ screens/TabTwoScreen.tsx
@@ -2,11 +2,14 @@ import { StyleSheet } from 'react-native';
 
 import EditScreenInfo from '../components/EditScreenInfo';
 import { Text, View } from '../components/Themed';
+import { useSelector } from 'react-redux';
+import { RootState } from '../state/redux';
 
 export default function TabTwoScreen() {
+  const count = useSelector((state: RootState) => state.counter.value)
   return (
     <View style={styles.container}>
-      <Text style={styles.title}>Tab Two</Text>
+      <Text style={styles.title}>{count}</Text>
       <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
       <EditScreenInfo path="/screens/TabTwoScreen.tsx" />
     </View>
